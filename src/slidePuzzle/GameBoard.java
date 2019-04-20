4fad748b6de0a87b3c2b9ae11471985908a16e35
Noah H <44982874+hindesn@users.noreply.github.com>
2019-04-20 11:49:34 -0400

    Update GameBoard.java

--- src/slidePuzzle/GameBoard.java
+++ src/slidePuzzle/GameBoard.java
@@ -22,8 +22,26 @@ public class GameBoard implements Game{
 
 	@Override
 	public void populateBoard(Tile t) {
-		// TODO Auto-generated method stub
+		populateBoard(t, 0, 0);
 		
+	}
+	
+	Private void populateBoard(Tile root, int rowStart, int colStart){
+
+		if(root instanceOf InternalTile){ //if internal node just call fcn recursively on children
+			generateIntArray(root.NW, rowStart, colStart);
+			generateIntArray(root.NE, rowStart, colStart + root.size()/2);
+			generateIntArray(root.SE, rowStart + root.size()/2, colStart + row.size()/2);
+			generateIntArray(root.SW, rowStart + root.size()/2, coStart);
+		}
+		
+		else{ //if leaf node:
+			for (int i = rowStart; i < rowStart + root.size(); i++){
+				for (int j = colStart; ij< colStart + root.size(); j++){
+					ordToPrint[i][j] = root.size();
+				}
+			}
+		}
 	}
 
 	@Override
