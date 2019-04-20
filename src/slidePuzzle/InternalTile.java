2f1b2c4dd227aac4a7f57147f3985cca6292d86f
Naomi <naomi.pohl@yahoo.com>
2019-04-20 12:21:27 -0400

    Update InternalTile.java

--- src/slidePuzzle/InternalTile.java
+++ src/slidePuzzle/InternalTile.java
@@ -6,16 +6,53 @@ public class InternalTile {
 	private Tile NW;
 	private Tile SE;
 	private Tile SW;
-	
+	private int depth;
+
 	public InternalTile(Tile NE, Tile NW, Tile SE, Tile SW) {
-		this.NE = NE;
-		this.NW = NW;
-		this.SE = SE;
-		this.SW = SW;
+		this.setNE(NE);
+		this.setNW(NW);
+		this.setSE(SE);
+		this.setSW(SW);
+	}
+
+	public int getDepth() {
+		return depth;
+	}
+
+	public void setDepth(int depth) {
+		this.depth = depth;
+	}
+
+	public Tile getSE() {
+		return SE;
+	}
+
+	public void setSE(Tile sE) {
+		SE = sE;
+	}
+
+	public Tile getNE() {
+		return NE;
+	}
+
+	public void setNE(Tile nE) {
+		NE = nE;
+	}
+
+	public Tile getNW() {
+		return NW;
+	}
+
+	public void setNW(Tile nW) {
+		NW = nW;
+	}
+
+	public Tile getSW() {
+		return SW;
+	}
+
+	public void setSW(Tile sW) {
+		SW = sW;
 	}
 }
 
-/*SIZE GETTER*/
-public int size() {
-	return this.size();
-}
