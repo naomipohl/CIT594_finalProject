42047e6c1219d3c317b3338c3a1a13e02478cfea
catherineweiss <cweisspa@gmail.com>
2019-04-23 19:48:14 -0400

    Update playGame.java
    added notes about error checking

--- src/slidePuzzle/PlayGame.java
+++ src/slidePuzzle/PlayGame.java
@@ -3,6 +3,16 @@ package slidePuzzle;
 public class PlayGame {
 	
 	public static void main(String[] args) {
+		
+		
+		//Error checking:
+		/*
+		merge() can only be performed if dogWalker.depth < maxDepth
+		split() can only be performed if dogWaler.depth > minDepth
+		*/
+		
+		
+		
 		// TODO Auto-generated method stub
 	}
 
