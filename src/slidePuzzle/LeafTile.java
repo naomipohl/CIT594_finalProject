42395b676cf796ce0de4e183bd9478e70fe0d563
Naomi <naomi.pohl@yahoo.com>
2019-04-29 13:28:52 -0400

    update merge

--- src/slidePuzzle/LeafTile.java
+++ src/slidePuzzle/LeafTile.java
@@ -27,7 +27,6 @@ public class LeafTile implements Tile {
 			this.startCol = parentStartCol + depth;
 			break;
 		}
-
 	}
 
 	public int getStartRow() {
@@ -46,6 +45,10 @@ public class LeafTile implements Tile {
 		return location;
 	}
 
+	public void setLocation(int i) {
+		this.location = i;
+	}
+
 	/**
 	 * Merges four sibling tiles into a single leaf tile. The parent internal tile
 	 * of the four sibling tiles becomes a leaf tile of the same depth and at same
@@ -54,10 +57,10 @@ public class LeafTile implements Tile {
 	 * 
 	 * @return new leaf tile
 	 */
-	public LeafTile merge(InternalTile parent, InternalTile grandparent) {
-
-		return parent.merge(parent, grandparent); // call merge on parent and update reference
+	public LeafTile merge(InternalTile parent, InternalTile grandparent, LeafTile l) {
+		// call merge on parent and update reference
 		// from grandparent
+		return parent.merge(parent, grandparent, l);
 	}
 
 	/**
@@ -72,46 +75,39 @@ public class LeafTile implements Tile {
 	}
 
 	/**
-	 * Split divides tile into four leaf tiles. If an internal tile is split, 
-	 * that internal tile is returned. If a leaf tile is split, the parent
-	 * of that original leaf tile is returned. It will be grandparent to the
-	 * four new leaf tiles.  
-	 * Split will not be performed if current depth <= minDepth. Error checking 
-	 * will be done in main method. 
+	 * Split divides tile into four leaf tiles. If an internal tile is split, that
+	 * internal tile is returned. If a leaf tile is split, the parent of that
+	 * original leaf tile is returned. It will be grandparent to the four new leaf
+	 * tiles. Split will not be performed if current depth <= minDepth. Error
+	 * checking will be done in main method.
 	 * 
-	 * @return Parent tile of original leaf tile (Grandparent of new four leaf tiles)
+	 * @return Parent tile of original leaf tile (Grandparent of new four leaf
+	 *         tiles)
 	 */
 	public InternalTile split(InternalTile parent) {
-		
+
 		// Create tile's 4 children
 		LeafTile NE = new LeafTile((this.depth / 2), 1, this.startRow, this.startCol);
 		LeafTile NW = new LeafTile((this.depth / 2), 2, this.startRow, this.startCol);
-		LeafTile SW = new LeafTile((this.depth / 2), 3, this.startRow, this.startCol); 
-		LeafTile SE = new LeafTile((this.depth / 2), 4, this.startRow, this.startCol); 
-		
+		LeafTile SW = new LeafTile((this.depth / 2), 3, this.startRow, this.startCol);
+		LeafTile SE = new LeafTile((this.depth / 2), 4, this.startRow, this.startCol);
+
 		// Turn current node into an internal node
-		InternalTile tile = new InternalTile(NE, NW, SE, SW);
+		InternalTile tile = new InternalTile(NE, NW, SW, SE);
 		tile.setDepth(this.depth);
 		tile.setLocation(this.location);
-		
+
 		if (this.location == 1) {
 			parent.setNE(tile);
-		}
-		else if (this.location == 2) {
+		} else if (this.location == 2) {
 			parent.setNW(tile);
-		}
-		else if (this.location == 3) { 
+		} else if (this.location == 3) {
 			parent.setSW(tile);
+		} else if (this.location == 4) {
+			parent.setSE(tile);
 		}
-		else {
-			parent.setSE(tile); 
-		}
-		
-		return parent;
-	}
 
-	public void setLocation(int i) {
-		this.location = i;
+		return parent;
 	}
 
 	@Override
@@ -124,7 +120,6 @@ public class LeafTile implements Tile {
 			return (LeafTile) l;
 		} else if (s.equals("sr")) {
 			Tile l = findNeighbor(root, this.getStartRow(), this.getStartCol() + this.getDepth());
-			System.out.println("LEAF IS: " + l);
 			return (LeafTile) l;
 		} else {
 			Tile l = findNeighbor(root, this.getStartRow(), this.getStartCol() - this.getDepth());
@@ -140,9 +135,6 @@ public class LeafTile implements Tile {
 
 		if (tile.isLeaf()) {
 			if (((LeafTile) tile).getStartRow() == rowToFind && ((LeafTile) tile).getStartCol() == colToFind) {
-				System.out.println("depth is: ");
-				System.out.println(((LeafTile) tile).getDepth());
-				System.out.println("returning tile: " + tile);
 				return tile;
 			}
 		} else {
@@ -165,4 +157,3 @@ public class LeafTile implements Tile {
 	}
 
 }
-
